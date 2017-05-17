a64a67976a5db3c6b5e1000866695ded22368c91
elt12ddo <elt12ddo@student.lth.se>
2017-05-17 12:28:03 +0200

    Done with ass2

--- Assignment2/Task1.m
+++ Assignment2/Task1.m
@@ -31,3 +31,18 @@ A = [2 0 0 0 0;
  options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'off');
  [x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options)
  fval = -fval
+ 
+ opt = x;
+ %There is no upper limit sice the optimal solution already uses the max
+ %amount of PI so the upper limit will be infinity
+ while 1
+     c(1) = c(1) + 0.001;
+      [x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options);
+      if ~isequal(x,opt)
+          break; 
+      end  
+ end;
+ c
+ x
+ 
+ %gives alpha = 0.6 and beta = inf
--- Assignment2/Task2.m
+++ Assignment2/Task2.m
@@ -24,11 +24,31 @@ c = -[1 5];
 A = [2 -1;
      -1 1;
      1 4];
+   %  -1 0
+   %  0 -1];
  b = [4;
       1;
       12];
+    %  -2;
+   %   -3];
  lb = zeros(2,1);
 
  options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'off');
  [x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options)
  fval = -fval
+ 
+ %%
+ % floor() and ceil()
+ %initial values on x1 and x2
+ objFunc = @(x) x(1)+5.*x(2);
+ x = [1.6;
+     2.6];
+ %initial upper bound
+ upBound = objFunc(x)
+ lowBound = objFunc(floor(x))
+ 
+ 
+ %if rem(value,1) == 0 => value is integer
+ 
+ 
+ 
